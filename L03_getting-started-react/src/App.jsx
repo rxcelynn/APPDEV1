8bbe85541a50959c5a4b7b05e2bd4b92a54701b9
rocelyn <rocelynlava57@gmail.com>
2026-07-18 15:02:23 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,40 @@
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>ABOUT ME</h1>
+      <div>
+        {/* Basic Info Section */}
+        <h1>Rocelyn Lava</h1>
+        <h2>BS Information Systems — 3rd Year</h2>
+        
+        <hr />
+
+        {/* Fun Fact Section */}
+        <h3>Fun Fact</h3>
+        <ul>
+          <li>One fun fact about me is that I am funny... sometimes!</li>
+        </ul>
+
+        <hr />
+
+        {/* Reflection Section */}
+        <h3>My Reflection</h3>
+        <p>
+          Learning this subject has been quite a journey, and honestly, it is difficult for me. 
+          I have always been curious about web applications—specifically, how they are built and how they function behind the scenes. 
+          While this course is finally helping me understand the things I am curious about, I am still low-key struggling with the setup. 
+          The online class modality makes it challenging for me to fully absorb everything, but I am trying my absolute best to stay focused and keep up. 
+          So far, I have really enjoyed learning how to use GitHub commands. 
+          *Nahihilo, nalilitooo~* literally, but there is a distinct sense of fulfillment whenever I figure out the mistakes I have made and fix them. 
+          Overcoming these technical hurdles is incredibly rewarding, and I know building this problem-solving resilience will be essential for my future career in Information Systems.
+        </p>
+      </div>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
